ebd3d69a119ceeaac5b8707f75e12249b2551e59
Sean O'Neill <s.oneill@f5.com>
2021-11-30 15:08:31 +0000

    Add support for VSR DOS config

--- internal/configs/virtualserver_test.go
+++ internal/configs/virtualserver_test.go
@@ -1370,6 +1370,352 @@ func TestGenerateVirtualServerConfigForVirtualServerWithMatches(t *testing.T) {
 	}
 }
 
+func TestGenerateVirtualServerConfigForVirtualServerRoutesWithDos(t *testing.T) {
+	dosResources := map[string]*appProtectDosResource{
+		"/coffee": {
+			AppProtectDosEnable:          "on",
+			AppProtectDosLogEnable:       false,
+			AppProtectDosMonitorUri:      "test.example.com",
+			AppProtectDosMonitorProtocol: "http",
+			AppProtectDosMonitorTimeout:  0,
+			AppProtectDosName:            "my-dos",
+			AppProtectDosAccessLogDst:    "svc.dns.com:123",
+			AppProtectDosPolicyFile:      "",
+			AppProtectDosLogConfFile:     "",
+		},
+	}
+
+	virtualServerEx := VirtualServerEx{
+		VirtualServer: &conf_v1.VirtualServer{
+			ObjectMeta: meta_v1.ObjectMeta{
+				Name:      "cafe",
+				Namespace: "default",
+			},
+			Spec: conf_v1.VirtualServerSpec{
+				Host: "cafe.example.com",
+				Upstreams: []conf_v1.Upstream{
+					{
+						Name:    "tea-v1",
+						Service: "tea-svc-v1",
+						Port:    80,
+					},
+					{
+						Name:    "tea-v2",
+						Service: "tea-svc-v2",
+						Port:    80,
+					},
+				},
+				Routes: []conf_v1.Route{
+					{
+						Path: "/tea",
+						Matches: []conf_v1.Match{
+							{
+								Conditions: []conf_v1.Condition{
+									{
+										Header: "x-version",
+										Value:  "v2",
+									},
+								},
+								Action: &conf_v1.Action{
+									Pass: "tea-v2",
+								},
+							},
+						},
+						Action: &conf_v1.Action{
+							Pass: "tea-v1",
+						},
+					},
+					{
+						Path:  "/coffee",
+						Route: "default/coffee",
+					},
+				},
+			},
+		},
+		Endpoints: map[string][]string{
+			"default/tea-svc-v1:80": {
+				"10.0.0.20:80",
+			},
+			"default/tea-svc-v2:80": {
+				"10.0.0.21:80",
+			},
+			"default/coffee-svc-v1:80": {
+				"10.0.0.30:80",
+			},
+			"default/coffee-svc-v2:80": {
+				"10.0.0.31:80",
+			},
+		},
+		VirtualServerRoutes: []*conf_v1.VirtualServerRoute{
+			{
+				ObjectMeta: meta_v1.ObjectMeta{
+					Name:      "coffee",
+					Namespace: "default",
+				},
+				Spec: conf_v1.VirtualServerRouteSpec{
+					Host: "cafe.example.com",
+					Upstreams: []conf_v1.Upstream{
+						{
+							Name:    "coffee-v1",
+							Service: "coffee-svc-v1",
+							Port:    80,
+						},
+						{
+							Name:    "coffee-v2",
+							Service: "coffee-svc-v2",
+							Port:    80,
+						},
+					},
+					Subroutes: []conf_v1.Route{
+						{
+							Path: "/coffee",
+							Matches: []conf_v1.Match{
+								{
+									Conditions: []conf_v1.Condition{
+										{
+											Argument: "version",
+											Value:    "v2",
+										},
+									},
+									Action: &conf_v1.Action{
+										Pass: "coffee-v2",
+									},
+								},
+							},
+							Dos: "test_ns/dos_protected",
+							Action: &conf_v1.Action{
+								Pass: "coffee-v1",
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	baseCfgParams := ConfigParams{}
+
+	expected := version2.VirtualServerConfig{
+		Upstreams: []version2.Upstream{
+			{
+				UpstreamLabels: version2.UpstreamLabels{
+					Service:           "tea-svc-v1",
+					ResourceType:      "virtualserver",
+					ResourceName:      "cafe",
+					ResourceNamespace: "default",
+				},
+				Name: "vs_default_cafe_tea-v1",
+				Servers: []version2.UpstreamServer{
+					{
+						Address: "10.0.0.20:80",
+					},
+				},
+			},
+			{
+				Name: "vs_default_cafe_tea-v2",
+				UpstreamLabels: version2.UpstreamLabels{
+					Service:           "tea-svc-v2",
+					ResourceType:      "virtualserver",
+					ResourceName:      "cafe",
+					ResourceNamespace: "default",
+				},
+				Servers: []version2.UpstreamServer{
+					{
+						Address: "10.0.0.21:80",
+					},
+				},
+			},
+			{
+				Name: "vs_default_cafe_vsr_default_coffee_coffee-v1",
+				UpstreamLabels: version2.UpstreamLabels{
+					Service:           "coffee-svc-v1",
+					ResourceType:      "virtualserverroute",
+					ResourceName:      "coffee",
+					ResourceNamespace: "default",
+				},
+				Servers: []version2.UpstreamServer{
+					{
+						Address: "10.0.0.30:80",
+					},
+				},
+			},
+			{
+				Name: "vs_default_cafe_vsr_default_coffee_coffee-v2",
+				UpstreamLabels: version2.UpstreamLabels{
+					Service:           "coffee-svc-v2",
+					ResourceType:      "virtualserverroute",
+					ResourceName:      "coffee",
+					ResourceNamespace: "default",
+				},
+				Servers: []version2.UpstreamServer{
+					{
+						Address: "10.0.0.31:80",
+					},
+				},
+			},
+		},
+		Maps: []version2.Map{
+			{
+				Source:   "$http_x_version",
+				Variable: "$vs_default_cafe_matches_0_match_0_cond_0",
+				Parameters: []version2.Parameter{
+					{
+						Value:  `"v2"`,
+						Result: "1",
+					},
+					{
+						Value:  "default",
+						Result: "0",
+					},
+				},
+			},
+			{
+				Source:   "$vs_default_cafe_matches_0_match_0_cond_0",
+				Variable: "$vs_default_cafe_matches_0",
+				Parameters: []version2.Parameter{
+					{
+						Value:  "~^1",
+						Result: "/internal_location_matches_0_match_0",
+					},
+					{
+						Value:  "default",
+						Result: "/internal_location_matches_0_default",
+					},
+				},
+			},
+			{
+				Source:   "$arg_version",
+				Variable: "$vs_default_cafe_matches_1_match_0_cond_0",
+				Parameters: []version2.Parameter{
+					{
+						Value:  `"v2"`,
+						Result: "1",
+					},
+					{
+						Value:  "default",
+						Result: "0",
+					},
+				},
+			},
+			{
+				Source:   "$vs_default_cafe_matches_1_match_0_cond_0",
+				Variable: "$vs_default_cafe_matches_1",
+				Parameters: []version2.Parameter{
+					{
+						Value:  "~^1",
+						Result: "/internal_location_matches_1_match_0",
+					},
+					{
+						Value:  "default",
+						Result: "/internal_location_matches_1_default",
+					},
+				},
+			},
+		},
+		HTTPSnippets:  []string{},
+		LimitReqZones: []version2.LimitReqZone{},
+		Server: version2.Server{
+			ServerName:  "cafe.example.com",
+			StatusZone:  "cafe.example.com",
+			VSNamespace: "default",
+			VSName:      "cafe",
+			InternalRedirectLocations: []version2.InternalRedirectLocation{
+				{
+					Path:        "/tea",
+					Destination: "$vs_default_cafe_matches_0",
+				},
+				{
+					Path:        "/coffee",
+					Destination: "$vs_default_cafe_matches_1",
+				},
+			},
+			Locations: []version2.Location{
+				{
+					Path:                     "/internal_location_matches_0_match_0",
+					ProxyPass:                "http://vs_default_cafe_tea-v2$request_uri",
+					ProxyNextUpstream:        "error timeout",
+					ProxyNextUpstreamTimeout: "0s",
+					ProxyNextUpstreamTries:   0,
+					Internal:                 true,
+					ProxySSLName:             "tea-svc-v2.default.svc",
+					ProxyPassRequestHeaders:  true,
+					ProxySetHeaders:          []version2.Header{{Name: "Host", Value: "$host"}},
+					ServiceName:              "tea-svc-v2",
+				},
+				{
+					Path:                     "/internal_location_matches_0_default",
+					ProxyPass:                "http://vs_default_cafe_tea-v1$request_uri",
+					ProxyNextUpstream:        "error timeout",
+					ProxyNextUpstreamTimeout: "0s",
+					ProxyNextUpstreamTries:   0,
+					Internal:                 true,
+					ProxySSLName:             "tea-svc-v1.default.svc",
+					ProxyPassRequestHeaders:  true,
+					ProxySetHeaders:          []version2.Header{{Name: "Host", Value: "$host"}},
+					ServiceName:              "tea-svc-v1",
+				},
+				{
+					Path:                     "/internal_location_matches_1_match_0",
+					ProxyPass:                "http://vs_default_cafe_vsr_default_coffee_coffee-v2$request_uri",
+					ProxyNextUpstream:        "error timeout",
+					ProxyNextUpstreamTimeout: "0s",
+					ProxyNextUpstreamTries:   0,
+					Internal:                 true,
+					ProxySSLName:             "coffee-svc-v2.default.svc",
+					ProxyPassRequestHeaders:  true,
+					ProxySetHeaders:          []version2.Header{{Name: "Host", Value: "$host"}},
+					ServiceName:              "coffee-svc-v2",
+					IsVSR:                    true,
+					VSRName:                  "coffee",
+					VSRNamespace:             "default",
+					Dos: &version2.Dos{
+						Enable:               "on",
+						Name:                 "my-dos",
+						ApDosMonitorUri:      "test.example.com",
+						ApDosMonitorProtocol: "http",
+						ApDosAccessLogDest:   "svc.dns.com:123",
+					},
+				},
+				{
+					Path:                     "/internal_location_matches_1_default",
+					ProxyPass:                "http://vs_default_cafe_vsr_default_coffee_coffee-v1$request_uri",
+					ProxyNextUpstream:        "error timeout",
+					ProxyNextUpstreamTimeout: "0s",
+					ProxyNextUpstreamTries:   0,
+					Internal:                 true,
+					ProxySSLName:             "coffee-svc-v1.default.svc",
+					ProxyPassRequestHeaders:  true,
+					ProxySetHeaders:          []version2.Header{{Name: "Host", Value: "$host"}},
+					ServiceName:              "coffee-svc-v1",
+					IsVSR:                    true,
+					VSRName:                  "coffee",
+					VSRNamespace:             "default",
+					Dos: &version2.Dos{
+						Enable:               "on",
+						Name:                 "my-dos",
+						ApDosMonitorUri:      "test.example.com",
+						ApDosMonitorProtocol: "http",
+						ApDosAccessLogDest:   "svc.dns.com:123",
+					},
+				},
+			},
+		},
+	}
+
+	isPlus := false
+	isResolverConfigured := false
+	vsc := newVirtualServerConfigurator(&baseCfgParams, isPlus, isResolverConfigured, &StaticConfigParams{MainAppProtectDosLoadModule: true}, false)
+
+	result, warnings := vsc.GenerateVirtualServerConfig(&virtualServerEx, nil, dosResources)
+	if diff := cmp.Diff(expected, result); diff != "" {
+		t.Errorf("GenerateVirtualServerConfig() mismatch (-want +got):\n%s", diff)
+	}
+
+	if len(warnings) != 0 {
+		t.Errorf("GenerateVirtualServerConfig returned warnings: %v", vsc.warnings)
+	}
+}
+
 func TestGenerateVirtualServerConfigForVirtualServerWithReturns(t *testing.T) {
 	virtualServerEx := VirtualServerEx{
 		VirtualServer: &conf_v1.VirtualServer{
